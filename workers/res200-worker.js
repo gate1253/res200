60c51c00a10816946d1a0c9946d0c3a9e41c2355
gate1253 <gway1253@gmail.com>
2025-11-09 14:16:10 +0900

    테스트

--- workers/res200-worker.js
+++ workers/res200-worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    const targetBaseUrl = "https://res302.gate1253.workers.dev/lFaj7V"; //env.TARGET_URL;
+    const targetBaseUrl = "https://res302.gate1253.workers.dev/api/list"; //env.TARGET_URL;
     
     // TARGET_URL이 슬래시로 끝나지 않으면 추가
     const initialUrl = targetBaseUrl;
