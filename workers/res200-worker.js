69286b88a4d0217470da96f1c7608c73db7df635
gate1253 <gway1253@gmail.com>
2025-11-09 12:08:08 +0900

    테스트

--- workers/res200-worker.js
+++ workers/res200-worker.js
@@ -9,14 +9,18 @@ export default {
       : `${targetBaseUrl}${url.pathname}${url.search}`;
 
     try {
-      // 원본 요청의 메서드, 헤더, 본문을 포함하여 fetch 호출
-      const response = await fetch(initialUrl, {
-        method: request.method, // 원본 요청 메서드 전달
-        headers: request.headers, // 원본 요청 헤더 전달
-        body: request.body, // 원본 요청 본문 전달 (GET/HEAD 요청에는 null)
-        redirect: "follow", // 중요: 리디렉션을 자동으로 따라감
+      // const response = await fetch(initialUrl, { // fetch 호출 주석 처리
+      //   method: request.method, // 원본 요청 메서드 전달
+      //   headers: request.headers, // 원본 요청 헤더 전달
+      //   body: request.body, // 원본 요청 본문 전달 (GET/HEAD 요청에는 null)
+      //   redirect: "follow", // 중요: 리디렉션을 자동으로 따라감
+      // });
+      // return response;
+
+      // targetBaseUrl을 JSON으로 응답
+      return new Response(JSON.stringify({ targetUrl: targetBaseUrl }), {
+        headers: { 'Content-Type': 'application/json' },
       });
-      return response;
     } catch (error) {
       console.error("Error fetching initial URL:", error);
       return new Response("Internal Server Error" + error, { status: 500 });
