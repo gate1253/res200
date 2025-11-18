189d21bb39149ebcbbf1eab360289598916ac212
gate1253 <gway1253@gmail.com>
2025-11-19 00:31:42 +0900

    TEST

--- workers/res200-worker.js
+++ workers/res200-worker.js
@@ -66,6 +66,7 @@ export async function handleRequest(request, env){
       malgnPlayer.setup({
         targetID: "player",
         video: {
+          thumbnail: "https://timgs.acs.wecandeo.com/thumb/513/20150724/17/2257_6c4bd_00000.jpg?udate=2025053002?udate=20220406",
           primaryKey: "Gate1253", // 필요시 동적으로 변경 가능
           title: "Gate1253", // 필요시 동적으로 변경 가능
           source: "${target}"
