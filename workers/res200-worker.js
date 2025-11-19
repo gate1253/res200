7ca8dc33235ee69ef330870d247484993b851e55
gate1253 <gway1253@gmail.com>
2025-11-19 22:38:07 +0900

    Player CSS 수정

--- workers/res200-worker.js
+++ workers/res200-worker.js
@@ -75,7 +75,7 @@ export async function handleRequest(request, env){
 <meta charset="UTF-8">
 <title>Play Content</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script defer src="malgnPlayer.js"></script>
+<script defer src="https://r2.ggm.kr/malgnPlayer.js"></script>
 <script>
 window.onload = function () {
 	malgnPlayer.setup({
